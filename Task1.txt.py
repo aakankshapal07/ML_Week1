7fe9f3711f9940232e9f0e6c9761423aeeb8aeb8
Aakanksha Pal <akanshapal849@gmail.com>
2025-05-01 19:27:40 +0530

    Update Task1.txt.py

--- Task1.txt.py
+++ Task1.txt.py
@@ -1,1 +1,7 @@
 Task 1 
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+interest = input("Enter your interest: ")
+
+print(f"\nHi, I'm {name}, {age} years old from {location}. I love {interest}!")
