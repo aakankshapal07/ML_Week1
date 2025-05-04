72883b9b8b13f0b21e27735e8dc6e88a7a3ba2aa
Aakanksha Pal <akanshapal849@gmail.com>
2025-05-04 15:24:34 +0530

    Update Task3,4.py

--- Task3.txt.py
+++ Task3.txt.py
@@ -1,1 +1,26 @@
-Task 3 
+ # Task 3 :
+student_dict = {
+    101: "Alice",
+    102: "Bob",
+    103: "Charlie"
+}
+
+def search_student(roll_number):
+    return student_dict.get(roll_number, "Student not found")
+
+print("Task 3: Student Search")
+print(search_student(102))  
+print()
+
+# Task 4: 
+football_players = {"Alice", "Bob", "David"}
+cricket_players = {"Bob", "Charlie", "Eve"}
+
+both = football_players & cricket_players
+only_one = (football_players ^ cricket_players)
+none = set(student_dict.values()) - (football_players | cricket_players)
+
+print("Task 4: Sports Participation")
+print(f"Play both: {both}")
+print(f"Play only one: {only_one}")
+print(f"Play none: {none}")
