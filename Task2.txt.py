75985d331a153139b0b7676557695aaec8643b18
Aakanksha Pal <akanshapal849@gmail.com>
2025-05-04 15:22:45 +0530

    Update Task2.py

--- Task2.txt.py
+++ Task2.txt.py
@@ -1,1 +1,12 @@
-Task 2 
+Task 2
+students = [
+    (101, "Alice", 18),
+    (102, "Bob", 19),
+    (103, "Charlie", 18)
+]
+
+print("Task 2: Student Information")
+for student in students:
+    roll, name, age = student
+    print(f"Roll No: {roll}, Name: {name}, Age: {age}")
+print()
